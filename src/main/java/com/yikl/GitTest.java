6c7a9986b552381db410c5cb563266eaffa27592
yikl <yikl_gz17797576200@163.com>
2023-01-12 17:55:22 +0800

    first commit

---
+++ src/main/java/com/yikl/GitTest.java
@@ -0,0 +1,13 @@
+package com.yikl;
+
+/**
+ * @author : -YikL-
+ * @version : 1.8
+ * @className : GitTest
+ * @date :2023/1/12 17:53
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("版本1");
+    }
+}
