e31a684dfb4e59dccc6ca2d4aecfcb46a54c4991
yikl <yikl_gz17797576200@163.com>
2023-01-13 11:18:13 +0800

    master push1

--- src/main/java/com/yikl/GitTest.java
+++ src/main/java/com/yikl/GitTest.java
@@ -15,7 +15,7 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("冲突合并");
-
         System.out.println("master-冲突合并");
+        System.out.println("push-test");
     }
 }
